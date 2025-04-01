4cfb997a5c07937dd75046df695571f1f26b196c
MohammedJabivullah <mohammadjabivullah07@gmail.com>
2025-04-01 14:34:38 +0530

    First Missing Positive Integer Problem.

--- src/FMPN.java
+++ src/FMPN.java
@@ -1,4 +1,8 @@
 public class FMPN {
+    //To find the First Missing Positive Integer in given array
+    //With time complexity O(N) and space complexity O(1)
+
+    //solution
     public static int missing(int[] arr){
         int i=0;
         while(i<arr.length){
@@ -22,6 +26,7 @@ public class FMPN {
         return arr.length+1;
     }
     public static void main(String[] args) {
+        //giving ⬇️ This array as in put ANS: 4
      int[] A = {3,-2,1,2,7};
         System.out.println(FMPN.missing(A));
     }
