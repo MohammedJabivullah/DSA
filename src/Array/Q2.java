4d6f79675d3192c95f12a1f40852e073c9ef5e5f
MohammedJabivullah <mohammadjabivullah07@gmail.com>
2026-01-07 19:10:27 +0530

    Question 1 Solved using PrefixSum

---
+++ src/Array/Q2.java
@@ -0,0 +1,14 @@
+package Array;
+
+//Question 2: Given N array elements, count no of equilibrium index.
+//An index is said to be equilibrium index if:
+//sum of all elements left of iᵗʰ index (sum[0, i-1]) = sum of all elements Right of iᵗʰ index (sum[i+1, N-1])
+// Note: if i == 0, leftSum = 0;
+//      if i == N-1, rightSum = 0;
+
+public class Q2 {
+
+    public static void main(String[] args) {
+
+    }
+}
