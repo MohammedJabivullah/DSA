fffd51dfe446c27636a974b9e2ee9bafa78e39df
MohammedJabivullah <mohammadjabivullah07@gmail.com>
2026-01-07 23:18:54 +0530

    Second Question on Finding count of Equilibrium index

--- src/Array/Q2.java
+++ src/Array/Q2.java
@@ -8,7 +8,44 @@ package Array;
 
 public class Q2 {
 
-    public static void main(String[] args) {
+    public int countEquilibrium(int []arr){
 
+        int n = arr.length;
+        int []prefixSum = new int[n];
+        prefixSum[0] = arr[0];
+
+        for(int i=1; i<n; i++){
+            prefixSum[i] = prefixSum[i-1] + arr[i];
+        }
+
+        int count = 0;
+
+        for(int i=0; i<n; i++){
+            int leftSum, rightSum;
+            if(i == 0){
+                leftSum = 0;
+            }else{
+                leftSum = prefixSum[i-1]; //i-1 :- left index of current position
+            }
+
+            rightSum = prefixSum[n-1] -prefixSum[i];
+
+            if(leftSum == rightSum){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int []arr = {-3, 2, 4, -1};
+        // example tests //{-7,1,5,2,-4,3,0};
+                        //{3,-1,2,-1,1,2,1};
+
+        Q2 obj = new Q2();
+        int count = obj.countEquilibrium(arr);
+
+        System.out.println(count);
     }
 }
